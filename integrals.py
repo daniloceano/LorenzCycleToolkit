38f4128f06c10453793e1eaaa94d13e1f3ee99a0
Danilo Couto d Souza <danilocoutodsouza@MacBook-Pro-de-Danilo.local>
2022-01-08 20:33:48 -0300

    Editing Integrals

--- integrals.py
+++ integrals.py
@@ -13,202 +13,139 @@ danilo.oceano@gmail.com
 
 """
 
-from derivatives import calc_delvar_delp
+# from derivatives import calc_delvar_delp
 import numpy as np
-import math
+# import math
 
-def calc_zonal_ave(var,min_lon,max_lon,min_lat,max_lat):
+def TrazpezoidalIntegration(VariableData,dimension):
     """
-    Makes zonal averages of a variable for all z levels and times 
+    Integrate data using the trapezoidal rule 
     
     Parameters
     ----------
-    var: xarray.Dataset
-        the variable to be integrated
-    lat: float
-        the latitude to be calculated the zonal average
-    lons: array
-        the corresponding array of longitudes for the variable    
-    min_lon, max_lon: float
-        minimum and maximum longitude to be calculated the zonal average        
+    VariableData: xarray.Dataset
+        arrays containing data to be integrated
+    dimension: string
+        the indexer used for the xarray coordante which the integration will
+        be performed
+   
+    Returns
+    -------
+    trapz: xarray.Dataset
+        Arrays with the integrated data
+    """
+    DimensionArray = VariableData[dimension].values # array containing either lats or lons 
+    trapz = 0
+    delta = np.deg2rad(np.abs((DimensionArray[-1]-DimensionArray[0])/(len(DimensionArray)-1)))
+    for i in DimensionArray:
+        if i == DimensionArray[0] or i == DimensionArray[-1]:
+            trapz += VariableData.sel({dimension:i})
+        else:
+            trapz += 2*VariableData.sel({dimension:i})
+    return trapz*(delta/2)
+
+def CalcZonalAverage(VariableData,LonName):
+    """
+    Computates zonal averages of a variable for all z levels and times 
+    
+    Parameters
+    ----------
+    VariableData: xarray.Dataset
+        arrays containing data to be integrated
+    LonName: string
+        the indexer used for the longitude variable in the xarray       
    
     Returns
     -------
     zonal_ave: xarray.Dataset
-        Arrays of zonal avreages for all latitudes in the passed varieble
+        Arrays of zonal avreages for all longitudes from the passed Dataset
     """
-        
-    # trimm data for desired lons
-    var = var.sel(lon=slice(min_lon,max_lon),lat=slice(max_lat,min_lat))
-    
-    # # integrate using trapezoidal rule
-    # lons, trapz =var.lon.values, 0
-    # dx = (lons[-1]-lons[0])/(len(lons))
-    # for lon in lons:
-    #     if lon == lons[0] or lon == lons[-1]:
-    #         trapz += var.sel(lon=lon)
-    #     else:
-    #         trapz += 2*var.sel(lon=lon)
-    # trapz = trapz*(dx/2)
-    # # take the zonal average
-    # zonal_ave = trapz/(lons[0]-lons[1])
-    
-    # integrate using numpy trapezoidal function
-    lons = var.lon.values
-    trapz = np.trapz(var,lons)
-    zonal_ave = trapz/(lons[0]-lons[1])
-    
-    # # makes zonal average 
-    # zonal_ave = var*0
-    # for lon in var.lon.values:
-    #     zonal_ave.loc[dict(lon=lon)] =  var.mean('lon')
-        
+    # Get latitude and logitude data
+    lons = VariableData[LonName]
+    rlons = np.deg2rad(lons)
+    # Integrate through longitude
+    trapz = TrazpezoidalIntegration(VariableData,LonName)
+    # Take the zonal average
+    zonal_ave = trapz/(rlons[-1]-rlons[0]).values
     return zonal_ave
 
-def calc_area_ave(var,min_lon,max_lon,min_lat,max_lat):
+def CalcAreaAverage(VariableData,LatName,LonName=None):
     """
-    Makes an area average of a variable for all z levels and times
+    Computates the Area Average of a function.
+    
+    The default is to computate the zonal average and then a meridional average.
+    If the input data is already some sort of zonal quantity (average or not),
+    simply set LonName to None
     
     Parameters
     ----------
-    var: xarray.Dataset
-        the variable to be integrated
-    min_lon, max_lon, minlat, min_lon: float
-        minimum and maximum longitude/latitude to be calculated the zonal average
-
+    VariableData: xarray.Dataset
+        arrays containing data to be integrated
+    LatName: string 
+        the indexer used for the latitude variable in the xarray 
+    LonName: string (optional)
+        the indexer used for the longitude variable in the xarray 
+   
     Returns
     -------
-    area_ave: xarray.Dataset
-        Dataset of the passed varieble avreages trough the area delimited by
-        min_lon, max_lon, min_lat and max_lat
-        Note that this function returns the CZ value in all grid points    
+    zonal_ave: xarray.Dataset
+        Arrays of area avreages for all latitudes and longitudes from
+        the passed Dataset
     """
-    
-    # # integrate using trapezoidal rule
-    # trapz = 0
-    # lons = var.lon
-    # dx = (lons[-1]-lons[0])/(len(lons))
-    # for lon in lons:
-    #     if lon == lons[0] or lon == lons[-1]:
-    #         trapz += (var.sel(lon=lon)*np.sin(var.sel(lon=lon)))
-    #     else:
-    #         trapz += 2*(var.sel(lon=lon)*np.sin(var.sel(lon=lon)))
-    # trapz = trapz*(dx/2)
-    
-    # trimm data for desired lats and lons
-    var = var.sel(lon=slice(min_lon,max_lon),lat=slice(max_lat,min_lat))
-    # calc zonal average
-    za = calc_zonal_ave(var,min_lon,max_lon,min_lat,max_lat)
-    lats = var.lat.values
-    trapz = np.trapz(za*np.cos(za))
-    area_ave = trapz/(lats[0]-lats[1])
-    
-    # # trimm data for desired lats and lons
-    # x = var.sel(lon=slice(min_lon,max_lon),lat=slice(max_lat,min_lat))
-    # # calc zonal average
-    # za = calc_zonal_ave(var,min_lon,max_lon,min_lat,max_lat)
-    # # makes area average
-    # area_ave = x*0
-    # for lat in x.lat.values:
-    #     area_ave.loc[dict(lat=lat)] = \
-    #             za.mean('lat')
-    
+    # Get latitude and logitude data
+    lats = VariableData[LatName]
+    rlats = np.deg2rad(lats)
+    if LonName:
+        # If LonName is provided, get zonal ave
+        zonal_ave = CalcZonalAverage(VariableData,LonName)
+    else:
+        zonal_ave = VariableData
+    # Take the area avearge
+    trapz = TrazpezoidalIntegration(zonal_ave*np.cos(rlats),LatName)
+    area_ave = trapz/(np.sin(rlats[0])-np.sin(rlats[-1])).values
     return area_ave
 
+# def calc_static_stability(temp,min_lon,max_lon,min_lat,max_lat):
+#     """
+#     Computates the static stability parameter sigma for all vertical levels
+#     and for the desired domain
+    
+#     Parameters
+#     ----------
+#     temp: xarray.Dataset
+#         temperature data in Kelvin
+#     min_lon, max_lon, minlat, min_lon: float
+#         minimum and maximum longitude/latitude to be calculated the zonal average
 
-def interga_P(var):
-    """
-    Intergate the provided variable trough its pressure levels
+#     Returns
+#     -------
+#     sigma: xarray.Dataset
+#         Dataset containing sigma values for all pressure levels and for the
+#         box specyfied by min_lon, max_lon, min_lat and max_lat    
     
-    Parameters
-    ----------
-    var: xarray.Dataset
-        the variable to be integrated
-    """
+#     """
+#     R = 287     # ideal gas cte.
+#     g = 9.81    # gravity
+#     cp = 1004.0 # specific heat of air at cte. pressure
     
-    # get pressure levels
-    levs = var.level.values
-    # sum trough pressure levels
-    # and 
-    sum_levs = 0
-    for lev in range((len(levs)-1),0,-1):
-        # multiply by 100 to get pressur elevels in pascals
-        dp = (levs[lev-1] - levs[lev])
-        #
-        sum_levs = sum_levs + (
-                    ((var.sel(level=levs[lev-1]) + \
-                    var.sel(level=levs[lev]))/2) * dp)
-        
-    return  sum_levs
+#     # breaks the equation into two terms
+#     # 1) g * T/cp
+#     tmp1 = temp*0
+#     for lev in temp.level.values:
+#         tmp1.loc[dict(level=lev)] = g * temp.sel(level=lev)/cp
 
-def interga_phi(var,min_lat,max_lat):
-    """
-    Intergate the provided variable trough latitude
+#     # 2)  (g*p/R) * (delT/delP)
+#     tmp2 = calc_delvar_delp(temp)
+#     tmp3 = temp*0
+#     for lev in temp.level.values:
+#         tmp3.loc[dict(level=lev)] = (g * lev/R) * tmp2.sel(level=lev)
     
-    Parameters
-    ----------
-    var: xarray.Dataset
-        the variable to be integrated
-    """
+#     # first term - second term
+#     var = tmp1 - tmp3
     
-    # get pressure levels
-    lats = var.lat.sel(lat=slice(max_lat,min_lat)).values
-    # sum trough pressure levels
-    # and 
-    sum_lats = 0
-    for lat in range((len(lats)-1),0,-1):
-        # multiply by 100 to get pressur elevels in pascals
-        dy = (lats[lat-1] - lats[lat])
-        #
-        sum_lats = sum_lats + (
-                    ((var.sel(lat=lats[lat-1]) + \
-                    var.sel(lat=lats[lat]))/2) * dy)
-        
-    return  sum_lats
-
-
-
-def calc_static_stability(temp,min_lon,max_lon,min_lat,max_lat):
-    """
-    Computates the static stability parameter sigma for all vertical levels
-    and for the desired domain
+#     sigma = calc_area_ave(var,min_lon,max_lon,min_lat,max_lat)
     
-    Parameters
-    ----------
-    temp: xarray.Dataset
-        temperature data in Kelvin
-    min_lon, max_lon, minlat, min_lon: float
-        minimum and maximum longitude/latitude to be calculated the zonal average
-
-    Returns
-    -------
-    sigma: xarray.Dataset
-        Dataset containing sigma values for all pressure levels and for the
-        box specyfied by min_lon, max_lon, min_lat and max_lat    
-    
-    """
-    R = 287     # ideal gas cte.
-    g = 9.81    # gravity
-    cp = 1004.0 # specific heat of air at cte. pressure
-    
-    # breaks the equation into two terms
-    # 1) g * T/cp
-    tmp1 = temp*0
-    for lev in temp.level.values:
-        tmp1.loc[dict(level=lev)] = g * temp.sel(level=lev)/cp
-
-    # 2)  (g*p/R) * (delT/delP)
-    tmp2 = calc_delvar_delp(temp)
-    tmp3 = temp*0
-    for lev in temp.level.values:
-        tmp3.loc[dict(level=lev)] = (g * lev/R) * tmp2.sel(level=lev)
-    
-    # first term - second term
-    var = tmp1 - tmp3
-    
-    sigma = calc_area_ave(var,min_lon,max_lon,min_lat,max_lat)
-    
-    return sigma
+#     return sigma
     
 #def calc_static_stability(temp,min_lon,max_lon,min_lat,max_lat):
 #    """
