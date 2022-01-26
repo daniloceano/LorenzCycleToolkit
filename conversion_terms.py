bb6c879e0bf415ad7d60d7b5ce9f56283e28dc11
Danilo Couto d Souza <danilo.oceano@gmail.com>
2022-01-25 23:44:35 -0300

    removed misplaced multiplication from Ca

--- conversion_terms.py
+++ conversion_terms.py
@@ -175,7 +175,7 @@ def Calc_Ca(VWindComponentData,PressureData,OmegaData,TemperatureData,
     # Integrate in pressure
     function = FirstTerm+SecondTerm
     Ca = -VerticalTrazpezoidalIntegration(function,PressureData,
-                                          VerticalCoordIndexer)*9.8
+                                          VerticalCoordIndexer)
     # Check if units are in accordance with expected and convert
     try: 
         Ca = Ca.metpy.convert_units('W/ m **2')
