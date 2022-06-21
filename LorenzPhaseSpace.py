1225c839bb30935e37e1a51955571d2e101ced0d
daniloceano <danilo.oceano@gmail.com>
2022-06-21 19:59:34 -0300

    updates

--- LorenzPhaseSpace.py
+++ LorenzPhaseSpace.py
@@ -101,28 +101,77 @@ def plotSurface():
     
     ax.plot_trisurf(x, y, z, cmap = cmocean.cm.curl, norm=norm)
 
+def MarkerSizeKe(df):
+    
+    msizes = [60,100,140,200,240]
+    intervals = [3e5,3.5e5,4.5e5,5e5]
+
+
+    sizes = []
+    for val in df['Ke']:
+        if val <= intervals[0]:
+            sizes.append(60)
+        elif val > intervals[0] and val <= intervals[1]:
+            sizes.append(100)
+        elif val > intervals[1] and val <= intervals[2]:
+            sizes.append(140)
+        elif val > intervals[2] and val <= intervals[3]:
+            sizes.append(200)
+        else:
+            sizes.append(240)
+    df['sizes'] = sizes
+    
+    # Plot legend
+    labels = ['< '+str(intervals[0]),
+              '< '+str(intervals[1]),
+              '< '+str(intervals[2]),
+              '< '+str(intervals[3]),
+              '> '+str(intervals[3])]
+    l1 = plt.scatter([],[],c='k', s=msizes[0],label=labels[0])
+    l2 = plt.scatter([],[], c='k', s=msizes[1],label=labels[1])
+    l3 = plt.scatter([],[],c='k', s=msizes[2],label=labels[2])
+    l4 = plt.scatter([],[],c='k', s=msizes[3],label=labels[3])
+    l5 = plt.scatter([],[],c='k', s=msizes[4],label=labels[4])
+    leg = plt.legend([l1, l2, l3, l4, l5], labels, ncol=1, frameon=True,
+                     fontsize=6, handlelength = 0.5, handleheight = 3,
+                     borderpad = 1.5, scatteryoffsets = [0.1],
+                handletextpad=1.5, title='Eddy Kinect Engery (Ke)', scatterpoints = 1, loc = 1,
+                bbox_to_anchor=(-0.22, -0.56, 0.5, 1),labelcolor = '#383838')
+    plt.setp(leg.get_title(), color='#383838')
+    plt.setp(leg.get_title(),fontsize=5)
+    for i in range(len(leg.legendHandles)):
+        leg.legendHandles[i].set_color('#383838')
+        leg.legendHandles[i].set_edgecolor('gray')
+    
+    return df
+
 def plot2D():
     
     x = smoothed['Ca']
     y = smoothed['Ck']
-    z = smoothed['RGe']
+    z = smoothed['Ge']
+    w = smoothed['Ke']
     
     plt.close('all')
     plt.plot(fig_size=(30,30))
     plt.gcf().subplots_adjust(bottom=0.15)
+    plt.gcf().subplots_adjust(right=0.98)
+    plt.gcf().subplots_adjust(left=0.135)
     ax = plt.gca()
-
+    
+    # Line plot
     ax.plot(x,y,'-',c='gray',zorder=2,linewidth=3)
+    
+    # Scatter plot
+    s = MarkerSizeKe(smoothed)['sizes']
     divnorm = colors.TwoSlopeNorm(vmin=-3, vcenter=0, vmax=8)
-    dots = ax.scatter(x,y,c=z,cmap=cmocean.cm.curl,s=100,zorder=100,
+    dots = ax.scatter(x,y,c=z,cmap=cmocean.cm.curl,s=s,zorder=100,
                       edgecolors='grey', norm=divnorm)
-    ax.set_xlabel('Conversion from zonal to eddy Kinetic Energy (Ck)',
-                  fontsize=10,labelpad=18,c='#383838')
-    ax.set_ylabel('Conversion from zonal to eddy Potential Energy (Ca)',
-                  fontsize=10,labelpad=15,c='#383838')
-    plt.tick_params(labelsize=8)
+    
+    
+    # Plot limits
     ax.set_xlim(-4,8)
-    ax.set_ylim(-20,8)
+    ax.set_ylim(-23,8)
     
     # Gradient lines in the center of the plot
     for i in range(7):
@@ -139,22 +188,21 @@ def plot2D():
         ax.axvline(x=0-(i/offsety),zorder=0+(i/5),linewidth=lw,
                alpha=alpha-(i/offsetalpha),c=c)
         
+    # Labels
+    ax.set_xlabel('Conversion from zonal to eddy Kinetic Energy (Ck)',
+                  fontsize=10,labelpad=18,c='#383838')
+    ax.set_ylabel('Conversion from zonal to eddy Potential Energy (Ca)',
+                  fontsize=10,labelpad=20,c='#383838')
+    plt.tick_params(labelsize=8)
+    
     # Colorbar
     cbar = plt.colorbar(dots, extend='both')
-    cbar.ax.set_ylabel('Residual of the generation of eddy Potential Energy',
+    cbar.ax.set_ylabel('Generation of eddy Potential Energy (Ge)',
                        rotation=270,fontsize=10,verticalalignment='bottom',
                        c='#383838',labelpad=20)
     for t in cbar.ax.get_yticklabels():
          t.set_fontsize(8)
 
-    # Marking start and end of the system
-    ax.text(x[0], y[0],'A',
-            zorder=101,fontsize=12,horizontalalignment='center',
-            verticalalignment='center')
-    ax.text(x.iloc[-1], y.iloc[-1], 'Z',
-            zorder=101,fontsize=12,horizontalalignment='center',
-            verticalalignment='center')
-    
     # Annotate plot
     system = outfile.split('/')[-1].split('_')[0]
     datasource = outfile.split('/')[-1].split('_')[1]
@@ -170,10 +218,10 @@ def plot2D():
             horizontalalignment='left',transform=ax.transAxes)
     ax.text(0.14,1.025,end,fontsize=8,c='#242424',
             horizontalalignment='left',transform=ax.transAxes)
-    ax.text(-0.09,0.1,'Eddy is providing potential energy \n to the mean flow',
+    ax.text(-0.11,0.1,'Eddy is providing potential energy \n to the mean flow',
             rotation=90,fontsize=6,horizontalalignment='center',c='#19616C',
             transform=ax.transAxes)
-    ax.text(-0.09,0.6,'Eddy is gaining potential energy \n from the mean flow',
+    ax.text(-0.11,0.6,'Eddy is gaining potential energy \n from the mean flow',
             rotation=90,fontsize=6,horizontalalignment='center',c='#CF6D66',
             transform=ax.transAxes)
     ax.text(0.17,-0.11,'Eddy is gaining kinetic energy \n from the mean flow',
@@ -188,6 +236,14 @@ def plot2D():
     ax.text(1.2,0.65,'Latent heat release feeds \n eddy potential energy',
             rotation=270,fontsize=6,horizontalalignment='center',c='#CF6D66',
             transform=ax.transAxes)
+    
+    # Marking start and end of the system
+    ax.text(x[0], y[0],'A',
+            zorder=101,fontsize=12,horizontalalignment='center',
+            verticalalignment='center')
+    ax.text(x.iloc[-1], y.iloc[-1], 'Z',
+            zorder=101,fontsize=12,horizontalalignment='center',
+            verticalalignment='center')
         
     fname = FigsDir+"LPS.png"
     plt.savefig(fname,dpi=500)
@@ -217,3 +273,5 @@ results from the main.py program.")
     plot2D()
     # plot3D()
     # plot3D(bw=True)
+    
+    
