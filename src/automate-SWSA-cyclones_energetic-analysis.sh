576838d5e4734171167d6b2d50b1bd25bc0fc04c
daniloceano <danilo.oceano@gmail.com>
2023-04-27 20:04:14 -0300

    Feat: file for automating running the program

---
+++ src/automate-SWSA-cyclones_energetic-analysis.sh
@@ -0,0 +1,17 @@
+
+track_dir='../../SWSA-cyclones_energetic-analysis/tracks_LEC-format/intense/'
+era5_dir='../../SWSA-cyclones_energetic-analysis/met_data/ERA5/DATA/'
+
+for file in "$track_dir"*
+do
+
+    filename=$(basename "$file")
+    id=$(echo "$filename" | sed 's/track_//')
+    
+    echo "File ID: $id"
+    cp $file ../inputs/track
+    
+    era5_file="${era5_dir}${id}_ERA5.nc"
+    python lorenz-cycle.py $era5_file -t -r -g    
+
+done
