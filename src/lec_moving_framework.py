7f9b260ceceaae3e7f962717315d42d05d12bec8
daniloceano <danilo.oceano@gmail.com>
2023-12-20 17:28:46 -0300

    added testing and improved modularity

--- src/lec_moving_framework.py
+++ src/lec_moving_framework.py
@@ -6,7 +6,7 @@
 #    By: daniloceano <danilo.oceano@gmail.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/19 17:32:55 by daniloceano       #+#    #+#              #
-#    Updated: 2023/12/20 15:23:59 by daniloceano      ###   ########.fr        #
+#    Updated: 2023/12/20 17:26:37 by daniloceano      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -31,6 +31,24 @@ from GenerationDissipationTerms import GenerationDissipationTerms
 from BoxData import BoxData
 from BudgetResidual import calc_budget_diff, calc_residuals
 
+def create_terms_dict(args):
+    """
+    Create a dictionary for storing computation results of various meteorological terms.
+
+    Args:
+        args (argparse.Namespace): Script arguments.
+
+    Returns:
+        dict: Dictionary with keys for each meteorological term initialized to empty lists.
+    """
+    energy_terms = ['Az', 'Ae', 'Kz', 'Ke']
+    conversion_terms = ['Cz', 'Ca', 'Ck', 'Ce']
+    boundary_terms = ['BAz', 'BAe', 'BKz', 'BKe', 'BΦZ', 'BΦE']
+    generation_dissipation_terms = ['Gz', 'Ge', 'Dz', 'De'] if not args.residuals else ['Gz', 'Ge']
+
+    terms = energy_terms + conversion_terms + boundary_terms + generation_dissipation_terms
+    return {term: [] for term in terms}
+
 def extract_wind_and_height_components(idata, variable_list_df, args):
     """
     Extract wind components and geopotential height from the dataset for a specific time.
@@ -49,14 +67,16 @@ def extract_wind_and_height_components(idata, variable_list_df, args):
 
     iu = (idata[u_var].compute() * units(variable_list_df.loc['Eastward Wind Component']['Units']).to('m/s'))
     iv = (idata[v_var].compute() * units(variable_list_df.loc['Northward Wind Component']['Units']).to('m/s'))
-    ihgt = (idata[geopotential_var].compute() * units(variable_list_df.loc[geopotential_var]['Units']))
+    ihgt = (idata[geopotential_var].compute() * units(
+        variable_list_df[variable_list_df['Variable'] == geopotential_var]['Units'].iloc[0])
+        )
     
     if args.geopotential:
         ihgt = (ihgt / g).metpy.convert_units('gpm')
 
     return iu, iv, ihgt
 
-def get_position_and_limits(args, t, track, iu_850, iv_850, izeta_850, ihgt_850, lat, lon):
+def get_position_and_limits(args, t, track, data850):
     """
     Determine the central position of the system at a specific time and the box dimensions.
     This function calculates the central latitude and longitude, as well as the dimensions
@@ -66,8 +86,7 @@ def get_position_and_limits(args, t, track, iu_850, iv_850, izeta_850, ihgt_850,
         args (argparse.Namespace): Script arguments.
         t (pd.Timestamp): Current time.
         track (pd.DataFrame): DataFrame containing the tracking information.
-        iu_850, iv_850, izeta_850, ihgt_850 (xr.DataArray): Data arrays for specific calculations.
-        lat, lon (xr.DataArray): Latitude and longitude data arrays.
+        data850 (dict): Dictionary containing meteorological data at 850 hPa.
 
     Returns:
         dict: Dictionary containing the central position (latitude, longitude) and limits (width, length) of the system.
@@ -87,8 +106,8 @@ def get_position_and_limits(args, t, track, iu_850, iv_850, izeta_850, ihgt_850,
 
     elif args.choose:
         # For the 'choose' option, interactively determine the box limits
-        # This part will depend on how you implement the interactive selection
-        limits = draw_box_map(iu_850, iv_850, izeta_850, ihgt_850, lat, lon, t)
+        iu_850, iv_850, izeta_850, ihgt_850 = data850['max_wind']['data'], data850['max_wind']['data'], data850['min_max_zeta']['data'], data850['min_hgt']['data']
+        limits = draw_box_map(iu_850, iv_850, izeta_850, ihgt_850, data850['lat'], data850['lon'], t)
         central_lat, central_lon = (limits['max_lat'] + limits['min_lat']) / 2, (limits['max_lon'] + limits['min_lon']) / 2
         width, length = limits['max_lon'] - limits['min_lon'], limits['max_lat'] - limits['min_lat']
 
@@ -115,6 +134,48 @@ def get_position_and_limits(args, t, track, iu_850, iv_850, izeta_850, ihgt_850,
     }
 
     return position, limits
+
+def construct_data850(izeta_850, ihgt_850, iwspd_850, lat, lon):
+    """
+    Construct a dictionary with key meteorological features at 850 hPa and their corresponding
+    latitude, longitude, and data.
+
+    Args:
+        izeta_850 (xr.DataArray): Vorticity data at 850 hPa.
+        ight_850 (xr.DataArray): Geopotential height data at 850 hPa.
+        iwspd_850 (xr.DataArray): Wind speed data at 850 hPa.
+        lat (xr.DataArray): Latitude data array.
+        lon (xr.DataArray): Longitude data array.
+
+    Returns:
+        dict: Dictionary containing meteorological features at 850 hPa with their coordinates and data.
+    """
+    # Find extremum coordinates for each feature
+    min_max_zeta_lat, min_max_zeta_lon = find_extremum_coordinates(izeta_850, lat, lon, 'min_max_zeta')
+    min_hgt_lat, min_hgt_lon = find_extremum_coordinates(ihgt_850, lat, lon, 'min_hgt')
+    max_wind_lat, max_wind_lon = find_extremum_coordinates(iwspd_850, lat, lon, 'max_wind')
+
+    data850 = {
+        'min_max_zeta': {
+            'latitude': min_max_zeta_lat,
+            'longitude': min_max_zeta_lon,
+            'data': izeta_850
+        },
+        'min_hgt': {
+            'latitude': min_hgt_lat,
+            'longitude': min_hgt_lon,
+            'data': ihgt_850
+        },
+        'max_wind': {
+            'latitude': max_wind_lat,
+            'longitude': max_wind_lon,
+            'data': iwspd_850
+        },
+        'lat': lat,
+        'lon': lon,
+    }
+
+    return data850 
 
 def compute_and_store_terms(box_obj, terms_dict):
     """
@@ -173,7 +234,7 @@ def compute_and_store_terms(box_obj, terms_dict):
 
     return terms_dict
 
-def finalize_results(times, terms_dict, args, results_sub_directory):
+def finalize_results(times, terms_dict, args, results_subdirectory):
     """
     Process and finalize results. 
     This includes creating a DataFrame with results and saving it to a CSV file.
@@ -183,10 +244,10 @@ def finalize_results(times, terms_dict, args, results_sub_directory):
         times (array-like): Array of times for the computation.
         terms_dict (dict): Dictionary containing computed terms.
         args (argparse.Namespace): Script arguments.
-        results_sub_directory (str): Directory to save results.
+        results_subdirectory (str): Directory to save results.
     """
     # Convert the terms_dict to DataFrame
-    df = pd.DataFrame(terms_dict, index=pd.to_datetime(times), orient ='columns',dtype = float)
+    df = pd.DataFrame(terms_dict, index=pd.to_datetime(times), dtype = float)
 
     # Estimating budget terms (∂X/∂t) using finite differences
     df = calc_budget_diff(df, times, args)
@@ -198,7 +259,7 @@ def finalize_results(times, terms_dict, args, results_sub_directory):
     # Constructing output filename
     method = 'track' if args.track else 'choose'
     outfile_name = args.outname if args.outname else ''.join(args.infile.split('/')[-1].split('.nc')) + f'_{method}'
-    outfile_path = os.path.join(results_sub_directory, f'{outfile_name}.csv')
+    outfile_path = os.path.join(results_subdirectory, f'{outfile_name}.csv')
 
     # Saving the DataFrame to a CSV file
     df.to_csv(outfile_path)
@@ -206,31 +267,31 @@ def finalize_results(times, terms_dict, args, results_sub_directory):
 
     # Save system position as a csv file for replicability
     out_track = out_track.rename(columns={'datestr':'time','central_lat':'Lat','central_lon':'Lon'})
-    output_trackfile =  results_sub_directory+outfile_name+'_track'
+    output_trackfile =  results_subdirectory+outfile_name+'_track'
     out_track.to_csv(output_trackfile, index=False, sep=";")
 
     return outfile_path, df
 
-def plot_results(outfile_path, results_sub_directory, args):
+def plot_results(outfile_path, results_subdirectory, args):
     """
     Plot the results using the specified plotting scripts or functions.
 
     Args:
         outfile_path (str): Path to the file containing the results.
-        results_sub_directory (str): Directory to save plots.
+        results_subdirectory (str): Directory to save plots.
         args (argparse.Namespace): Script arguments.
     """
     # Example plotting commands, replace with actual plotting functions or scripts
     plot_flag = ' -r' if args.residuals else ''
     os.system(f"python ../plots/plot_timeseries.py {outfile_path}{plot_flag}")
-    os.system(f"python ../plots/plot_vertical.py {results_sub_directory}")
-    os.system(f"python ../plots/plot_boxplot.py {results_sub_directory}{plot_flag}")
+    os.system(f"python ../plots/plot_vertical.py {results_subdirectory}")
+    os.system(f"python ../plots/plot_boxplot.py {results_subdirectory}{plot_flag}")
     os.system(f"python ../plots/plot_LEC.py {outfile_path}")
     os.system(f"python ../plots/plot_LPS.py {outfile_path}")
     os.system(f"python ../plots/plot_track.py {outfile_path}")
 
-def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Dataset,
-               results_sub_directory: str, figures_directory: str,
+def lec_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Dataset,
+               results_subdirectory: str, figures_directory: str,
                args: argparse.Namespace):
     """
     Computes the Lorenz Energy Cycle using a moving (semi-lagrangian) framework.
@@ -239,7 +300,7 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
         data (xr.Dataset): A Xarray Dataset containing the data to compute the energy cycle.
         variable_list_df (pd.DataFrame): DataFrame with variable mappings.
         dTdt (xr.Dataset): Dataset containing temperature gradient data.
-        results_sub_directory (str): Directory for saving results.
+        results_subdirectory (str): Directory for saving results.
         figures_directory (str): Directory for saving figures.
         args (argparse.Namespace): Arguments provided to the script.
 
@@ -263,7 +324,7 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
         columns = [TimeName] + [float(i) for i in PressureData.values]
         df = pd.DataFrame(columns=columns)
         file_name = term + '_' + VerticalCoordIndexer + '.csv'
-        file_path = os.path.join(results_sub_directory, file_name)
+        file_path = os.path.join(results_subdirectory, file_name)
         df.to_csv(file_path, index=None) 
         logging.info(f'{file_path} created (but still empty)')
 
@@ -282,9 +343,7 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
     out_track = pd.DataFrame(columns=results_keys)
 
     # Dictionary for storing results
-    terms_dict = {term: [] for term in 
-                  ['Az', 'Ae', 'Kz', 'Ke', 'Cz', 'Ca', 'Ck', 'Ce', 'Ge', 'Gz', 
-                   'BAz', 'BAe', 'BKz', 'BKe', 'BΦZ', 'BΦE', 'Dz', 'De']}
+    terms_dict = create_terms_dict(args)
 
     # Slice the time array to match the track file
     times = pd.to_datetime(data[TimeName].values)
@@ -317,11 +376,12 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
         # Compute wind speed and vorticity at 850 hPa
         iwspd_850, izeta_850 = wind_speed(iu_850, iv_850), vorticity(iu_850, iv_850).metpy.dequantify()
         lat, lon = idata[LatIndexer], idata[LonIndexer]
+        data850 = construct_data850(izeta_850, ihgt_850, iwspd_850, lat, lon)
 
         # Get current time attributes
-        position, limits = get_position_and_limits(args, t, track, iu_850, iv_850, izeta_850, ihgt_850, lat, lon)
+        position, limits = get_position_and_limits(args, t, track, data850)
         out_track = out_track.append(position, ignore_index=True)
-        plot_domain_attributes(iu_850, iv_850, izeta_850, ihgt_850, lat, lon, t, figures_directory)
+        plot_domain_attributes(data850, position, figures_directory)
 
         # Create box object
         try:
@@ -333,7 +393,7 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
                 eastern_limit=limits['max_lon'],
                 southern_limit=limits['min_lat'],
                 northern_limit=limits['max_lat'],
-                output_dir=results_sub_directory,
+                output_dir=results_subdirectory,
                 dTdt=idTdt
             )
         except Exception as e:
@@ -344,7 +404,40 @@ def LEC_moving(data: xr.Dataset, variable_list_df: pd.DataFrame, dTdt: xr.Datase
         terms_dict = compute_and_store_terms(box_obj, terms_dict)
 
     # Finalize and process results
-    df, outfile_path = finalize_results(times, terms_dict, args, results_sub_directory)
+    df, outfile_path = finalize_results(times, terms_dict, args, results_subdirectory)
 
     if args.plots:
-        plot_results(outfile_path, results_sub_directory, args)
+        plot_results(outfile_path, results_subdirectory, args)
+
+if __name__ == '__main__':
+    from tools import prepare_data
+
+    args = argparse.Namespace(
+        infile='../samples/Reg1-Representative_NCEP-R2.nc',
+        residuals=True,
+        fixed=False,
+        geopotential=False,
+        track=True,
+        choose=False,
+        zeta=False,
+        mpas=False,
+        plots=False,
+        outname=None,
+        verbosity=False
+    )
+
+    initialize_logging()
+
+    varlist = "../inputs/fvars_NCEP-R2"
+    variable_list_df = pd.read_csv(varlist, sep=';', index_col=0, header=0)
+
+    data, _ = prepare_data(args, varlist)
+
+    dTdt =  data[variable_list_df.loc['Air Temperature']['Variable']].differentiate(
+                variable_list_df.loc['Time']['Variable'],datetime_unit='s') * units('K/s')
+
+    results_subdirectory = "../LEC_Results"
+    figures_directory = f"{results_subdirectory}/figures"
+    os.makedirs(results_subdirectory, exist_ok=True)
+
+    lec_moving(data, variable_list_df, dTdt, results_subdirectory, figures_directory, args)
