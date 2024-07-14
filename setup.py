c34072e08669e6c2e30644f1967b6c5220cf530f
daniloceano <danilo.oceano@gmail.com>
2024-07-14 18:59:07 -0300

    Update setup.py to read from requirements.txt and update GitHub Actions workflow

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
 from setuptools import setup, find_packages
-import pkg_resources
+import os
 
-def parse_requirements(filename):
-    with open(filename, 'r') as file:
-        return [str(req) for req in pkg_resources.parse_requirements(file)]
+# Utility function to read the requirements.txt file
+def read_requirements():
+    requirements_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    with open(requirements_path, 'r') as f:
+        return f.read().splitlines()
 
 setup(
     name='LorenzCycleToolkit',
-    version='0.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=parse_requirements('requirements.txt'),
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
-            'lorenzcycletoolkit=lorenzcycletoolkit.main:main',  # Adjust this as necessary
+            'lorenzcycletoolkit = lorenzcycletoolkit:main',
         ],
     },
-    author='Danilo Couto de Souza',
-    author_email='danilo.oceano@example.com',
-    description='A toolkit for computing the Lorenz Energy Cycle in atmospheric regions.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/daniloceano/LorenzCycleToolkit',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.12',
 )
